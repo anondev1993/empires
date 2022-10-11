5bb517940122e7f212d907a730d9a2ce770945b8
anondev <kycbored@gmail.com>
2022-10-11 17:42:23 +0200

    fix: advance time

--- empires_cli/utils/advanceTime.js
+++ empires_cli/utils/advanceTime.js
@@ -1,9 +1,12 @@
 async function advanceTime(timeInSeconds) {
+    const time = parseInt(timeInSeconds, 10);
     const response = await fetch("http://127.0.0.1:5050/increase_time", {
-        method: "POST",
-        body: {
-            time: timeInSeconds,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
         },
+        method: "POST",
+        body: JSON.stringify({ time: time }),
     });
     const res = await response.json();
     return res;
