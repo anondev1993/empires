b720c569d0c4ef5773ec546b10f02b95697fccd8
anondev <kycbored@gmail.com>
2022-10-11 17:42:04 +0200

    feat: resources + quickstart

--- empires_cli/index.js
+++ empires_cli/index.js
@@ -8,14 +8,47 @@ const {
     mintLordsUser,
     mintLordsEmpire,
 } = require("./cli/mint");
-const { ownerOf, readEmpire, readRealms } = require("./cli/read");
+const {
+    ownerOf,
+    readEmpire,
+    readRealms,
+    balanceOfBatch,
+} = require("./cli/read");
 const { joinEmpire, manageEmpire } = require("./cli/write");
 const { advanceTime } = require("./utils/advanceTime");
 const { getUserAccounts, getAdminAccount } = require("./utils/getAccount");
+const { getDeployedAddresses } = require("./utils/getAddresses.js");
+const { getDeployedContractAddress } = require("./utils/trackContracts.js");
+
+program
+    .command("quickstart")
+    .description(
+        "deploy a quick start version of all contracts for Realm and Empire, including minting of 3 tokens."
+    )
+    .action(async () => {
+        console.log("Deploying realms contract");
+        await deployRealmsContracts();
+        console.log("Deploying empire contract");
+        await deployEmpire();
+        console.log("Minting 1000 resources for the Empires");
+        await batchMintResourcesEmpire(1000);
+        console.log(
+            "Minting Realms NFTS for user 1 with tokenid 1 and joining empire"
+        );
+        await mintingRealm(1, 1);
+        await joinEmpire(1, 1);
+        console.log(
+            "Minting Realms NFTS for user 2 with tokenid 2 and joining empire"
+        );
+        await mintingRealm(2, 2);
+        await joinEmpire(2, 2);
+        console.log("Minting Realms NFTS for user 3 with tokenid 3");
+        await mintingRealm(3, 3);
+    });
 
 program
     .command("deploy-realms")
-    .description("Deploys all the necessary contracts to play Realms")
+    .description("deploy all the necessary contracts to play Realms")
     .action(() => {
         console.log("Deploying realms contract");
         deployRealmsContracts();
@@ -24,7 +57,7 @@ program
 program
     .command("deploy-empire")
     .description(
-        "Deploys the Empire contract using the Realms contract addresses"
+        "deploy the Empire contract using the Realms contract addresses"
     )
     .action(() => {
         console.log("Deploying empire contract");
@@ -33,7 +66,7 @@ program
 
 program
     .command("advance-time")
-    .description("advances the time of the devnet by an amount of seconds")
+    .description("advance the time of the devnet by an amount of seconds")
     .requiredOption(
         "-s, --seconds <seconds>",
         "the number of seconds to advance"
@@ -45,7 +78,7 @@ program
 
 program
     .command("show-users")
-    .description("Shows the users accounts")
+    .description("show the users accounts")
     .action(() => {
         console.log("Showing all the users: ");
         getUserAccounts();
@@ -53,7 +86,7 @@ program
 
 program
     .command("show-admin")
-    .description("Shows the admin account")
+    .description("show the admin account")
     .action(() => {
         console.log("Showing the admin account: ");
         getAdminAccount();
@@ -65,7 +98,7 @@ program
     .option("-u, --users <users...>", "if you want to mint batch for the users")
     .argument("<amount>", "an amount of resources to mint (in 10**18)")
     .description(
-        "Minting amount of all Realms resources for empire or defined users"
+        "mint amount of all Realms resources for empire or defined users"
     )
     .action((amount, options) => {
         if (options.empire) {
@@ -88,7 +121,7 @@ program
         "user that will receive the minted realm"
     )
     .requiredOption("-t, --tokenid <tokenid>", "the token id of the realm")
-    .description("Minting 1 Realms NFT with token id for one user")
+    .description("mint 1 Realms NFT with token id for one user")
     .action((options) => {
         console.log(
             `Minting Realms NFTS for user ${options.user} with tokenid ${options.tokenid}`
@@ -101,6 +134,7 @@ program
     .argument("<amount>", "an amount of lords to mint (in 10**18)")
     .option("-e, --empire", "if you want to mint lords for the empire")
     .option("-u, --user <user>", "if you want to mint lords for one user")
+    .description("mint an amount of $LORDS for the user")
     .action((amount, options) => {
         if (options.empire) {
             console.log(`Minting ${amount} of LORDS for empire`);
@@ -122,7 +156,7 @@ program
         "-t, --tokenid <tokenid>",
         "token if of the realm that joins empires"
     )
-    .description("Joins the empire by transferring realms nft")
+    .description("join the empire by transferring realms nft")
     .action((options) => {
         console.log(
             `User ${options.user} with token id ${options.tokenid} joins the empire`
@@ -133,8 +167,29 @@ program
 program
     .command("owner")
     .requiredOption("-t, --tokenid <tokenid>", "the id of the token")
+    .description("query the owner of the token")
     .action((options) => {
         ownerOf(options.tokenid);
+    });
+
+program
+    .command("resources")
+    .option("-u, --user <user>", "if you want to check the resources of a user")
+    .option("-e, --empire", "if you want to check the resources of the empire")
+    .description("query the current resources of the user")
+    .action(async (options) => {
+        if (options.empire) {
+            console.log("Querying the empires' resources");
+            const empireAddress = getDeployedContractAddress("empire");
+            balanceOfBatch(empireAddress);
+        } else {
+            if (options.user) {
+                console.log(`Querying user ${options.user}'s resources`);
+                const accountContracts = await getDeployedAddresses();
+                const index = parseInt(options.user, 10);
+                balanceOfBatch(accountContracts[index].address);
+            }
+        }
     });
 
 program
@@ -151,7 +206,7 @@ program
         "-c, --calldata <calldata>",
         "The arguments of the function as a string of a list (no space between elements)"
     )
-    .description("calls the functions from the empire contract")
+    .description("call the functions from the empire contract")
     .action((options) => {
         console.log(
             `Calls the function ${
@@ -178,6 +233,7 @@ program
         "-c, --calldata <calldata>",
         "The arguments of the function as a string of a list (no space between elements)"
     )
+    .description("query the storage of the Empire")
     .action(({ entrypoint, calldata }) => {
         readEmpire(entrypoint, JSON.parse(calldata));
     });
@@ -196,6 +252,7 @@ program
         "-c, --calldata <calldata>",
         "The arguments of the function as a string of a list (no space between elements)"
     )
+    .description("query the storage of the Realm module")
     .action(({ module, entrypoint, calldata }) => {
         readRealms(module, entrypoint, JSON.parse(calldata));
     });
